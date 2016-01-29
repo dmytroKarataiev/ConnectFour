14222b941a22df2f255e3309455cd3ab3bae4d02
dmytroKarataiev <karataev.dima@gmail.com>
2016-01-28 22:02:05 -0800

    refactor: delete redundancy, onBackPressed

--- app/src/main/java/karataiev/dmytro/connectfour/NewGame.java
+++ app/src/main/java/karataiev/dmytro/connectfour/NewGame.java
@@ -1,9 +1,6 @@
 package karataiev.dmytro.connectfour;
 
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,54 +8,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import skeleton.SkeletonActivity;
 
 
-public class NewGame extends AppCompatActivity // implements
-        // GoogleApiClient.OnConnectionFailedListener,
-        // View.OnClickListener
-{
+public class NewGame extends AppCompatActivity {
 
     Button newGame, vsRandom, vsPlayer, vsHard, vsAdvanced, skeleton;
-
-    private static final String TAG = "SignInActivity";
-    private static final int RC_SIGN_IN = 9001;
-    GoogleApiClient mGoogleApiClient;
-    private TextView mStatusTextView;
-    private ProgressDialog mProgressDialog;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_game);
         addListenerOnButton();
-
-        // Views
-        mStatusTextView = (TextView) findViewById(R.id.status);
-
-        // Button listeners
-//        findViewById(R.id.sign_in_button).setOnClickListener(this);
-//        findViewById(R.id.sign_out_button).setOnClickListener(this);
-//        findViewById(R.id.disconnect_button).setOnClickListener(this);
-
-//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                .requestEmail()
-//                .build();
-//
-//        mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-//                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-//                .build();
-
-        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
-//        findViewById(R.id.sign_in_button).setOnClickListener(this);
-
     }
 
     // button initialization (vsEasy, vsRandom, vsPlayer)
@@ -74,7 +36,6 @@ public class NewGame extends AppCompatActivity // implements
                 Intent intent = new Intent(context, MainActivity.class);
                 intent.putExtra("player", 1);
                 startActivity(intent);
-
             }
 
         });
@@ -88,7 +49,6 @@ public class NewGame extends AppCompatActivity // implements
                 Intent intent = new Intent(context, MainActivity.class);
                 intent.putExtra("player", 2);
                 startActivity(intent);
-
             }
 
         });
@@ -102,7 +62,6 @@ public class NewGame extends AppCompatActivity // implements
                 Intent intent = new Intent(context, MainActivity.class);
                 intent.putExtra("player", 3);
                 startActivity(intent);
-
             }
 
         });
@@ -116,7 +75,6 @@ public class NewGame extends AppCompatActivity // implements
                 Intent intent = new Intent(context, MainActivity.class);
                 intent.putExtra("player", 4);
                 startActivity(intent);
-
             }
 
         });
@@ -130,12 +88,11 @@ public class NewGame extends AppCompatActivity // implements
                 Intent intent = new Intent(context, MainActivity.class);
                 intent.putExtra("player", 5);
                 startActivity(intent);
-
             }
 
         });
 
-        skeleton = (Button) findViewById(R.id.skeleton);
+        skeleton = (Button) findViewById(R.id.multiplayer);
         skeleton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -144,165 +101,7 @@ public class NewGame extends AppCompatActivity // implements
             }
         });
 
-
     }
-
-    /**
-     * check if back button was pressed and confirm exiting
-     */
-    @Override
-    public void onBackPressed() {
-        new AlertDialog.Builder(this)
-                .setTitle("Exit the game?")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-
-                })
-                .setNegativeButton("No", null)
-                .show();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-//        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
-//        if (opr.isDone()) {
-//            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
-//            // and the GoogleSignInResult will be available instantly.
-//            Log.d(TAG, "Got cached sign-in");
-//            GoogleSignInResult result = opr.get();
-//            handleSignInResult(result);
-//        } else {
-//            // If the user has not previously signed in on this device or the sign-in has expired,
-//            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
-//            // single sign-on will occur in this branch.
-//            showProgressDialog();
-//            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
-//                @Override
-//                public void onResult(GoogleSignInResult googleSignInResult) {
-//                    hideProgressDialog();
-//                    handleSignInResult(googleSignInResult);
-//                }
-//            });
-//        }
-
-//    // [START onActivityResult]
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-//        if (requestCode == RC_SIGN_IN) {
-//            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-//            handleSignInResult(result);
-//        }
-//    }
-//    // [END onActivityResult]
-//
-//    // [START handleSignInResult]
-//    private void handleSignInResult(GoogleSignInResult result) {
-//        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-//        if (result.isSuccess()) {
-//            // Signed in successfully, show authenticated UI.
-//            GoogleSignInAccount acct = result.getSignInAccount();
-//            mStatusTextView.setText("signed in");
-//            updateUI(true);
-//        } else {
-//            // Signed out, show unauthenticated UI.
-//            updateUI(false);
-//        }
-//    }
-//    // [END handleSignInResult]
-//
-//    // [START signIn]
-//    private void signIn() {
-//        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-//        startActivityForResult(signInIntent, RC_SIGN_IN);
-//    }
-//    // [END signIn]
-//
-//    // [START signOut]
-//    private void signOut() {
-//        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
-//                new ResultCallback<Status>() {
-//                    @Override
-//                    public void onResult(Status status) {
-//                        // [START_EXCLUDE]
-//                        updateUI(false);
-//                        // [END_EXCLUDE]
-//                    }
-//                });
-//    }
-//    // [END signOut]
-//
-//    // [START revokeAccess]
-//    private void revokeAccess() {
-//        Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
-//                new ResultCallback<Status>() {
-//                    @Override
-//                    public void onResult(Status status) {
-//                        // [START_EXCLUDE]
-//                        updateUI(false);
-//                        // [END_EXCLUDE]
-//                    }
-//                });
-//    }
-//    // [END revokeAccess]
-//
-//    @Override
-//    public void onConnectionFailed(ConnectionResult connectionResult) {
-//        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
-//        // be available.
-//        Log.d(TAG, "onConnectionFailed:" + connectionResult);
-//    }
-//
-//    private void showProgressDialog() {
-//        if (mProgressDialog == null) {
-//            mProgressDialog = new ProgressDialog(this);
-//            mProgressDialog.setMessage("loading");
-//            mProgressDialog.setIndeterminate(true);
-//        }
-//
-//        mProgressDialog.show();
-//    }
-//
-//    private void hideProgressDialog() {
-//        if (mProgressDialog != null && mProgressDialog.isShowing()) {
-//            mProgressDialog.hide();
-//        }
-//    }
-//
-//    private void updateUI(boolean signedIn) {
-//        if (signedIn) {
-//            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
-//            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
-//        } else {
-//            mStatusTextView.setText("signed out");
-//
-//            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
-//            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
-//        }
-//    }
-//
-//    @Override
-//    public void onClick(View v) {
-//        switch (v.getId()) {
-//            case R.id.sign_in_button:
-//                signIn();
-//                break;
-//            case R.id.sign_out_button:
-//                signOut();
-//                break;
-//            case R.id.disconnect_button:
-//                revokeAccess();
-//                break;
-//        }
-//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
